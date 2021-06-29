7517aaa5ee239745733fbb4c2600925096cd9e4e
MatheusA4180 <matheusaugusto4180@gmail.com>
2021-06-29 16:12:48 -0300

    feature - menu de navegação e appbar(faltando alguns ajustes)

--- app/src/main/java/com/example/tecnobank/home/HomeActivity.kt
+++ app/src/main/java/com/example/tecnobank/home/HomeActivity.kt
@@ -2,12 +2,34 @@ package com.example.tecnobank.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.tecnobank.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
+
         supportActionBar?.hide()
+        //setSupportActionBar(findViewById(R.id.toolbar))
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.appbar_menu, menu)
+        return true
     }
 }
