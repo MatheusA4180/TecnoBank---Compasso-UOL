662f8204565f317ea0f3d98b51235974467ef60e
samuell <samuelltaylor@gmail.com>
2021-06-30 11:45:43 -0300

    implementação completa api

--- app/src/main/java/com/example/tecnobank/intro/repository/LoginRepository.kt
+++ app/src/main/java/com/example/tecnobank/intro/repository/LoginRepository.kt
@@ -1,4 +1,31 @@
 package com.example.tecnobank.intro.repository
 
-class LoginRepository {
+import com.example.tecnobank.intro.data.remote.EndPoint
+import com.example.tecnobank.intro.data.remote.model.login.LoginPayload
+import com.example.tecnobank.intro.data.remote.model.login.LoginResponse
+import java.lang.Exception
+
+class LoginRepository(val endPoint: EndPoint) {
+
+    suspend fun login(email:String, password:String): LoginResponse{
+
+        val response = endPoint.login(LoginPayload(email,password))
+        if (response.isSuccessful)
+        {
+            return response.body()!!
+        }
+        else if(response.code()==401)
+        {
+            throw Exception("E-mail ou senha inválida.")
+        }
+        else if(response.code()==404)
+        {
+            throw Exception("E-mail não encontrado")
+        }
+        else
+        {
+            throw Exception("Falha no sistema.")
+        }
+    }
+
 }
--- app/src/main/java/com/example/tecnobank/intro/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/tecnobank/intro/viewmodel/LoginViewModel.kt
@@ -1,7 +1,27 @@
 package com.example.tecnobank.intro.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tecnobank.intro.repository.LoginRepository
+import kotlinx.coroutines.launch
 
-class LoginViewModel:ViewModel() {
+class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel() {
+
+    fun onLoginClicked() {
+        viewModelScope.launch {
+            try{
+                val loginResponse = loginRepository.login("", "")
+                //Tratar fluxo sucesso!
+            }
+            catch (e:Exception)
+            {
+                //tratar fluxo de erro!
+            }
+
+        }
+
+
+
+    }
 
 }
