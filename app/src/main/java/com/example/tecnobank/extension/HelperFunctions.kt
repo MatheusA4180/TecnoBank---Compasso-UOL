ea70878a91a022f6e8497cda407d261f36431b03
samuell <samuelltaylor@gmail.com>
2021-07-21 15:59:29 -0300

    Melhoria da HelperFunctions para mostrar as datas.

--- app/src/main/java/com/example/tecnobank/extension/HelperFunctions.kt
+++ app/src/main/java/com/example/tecnobank/extension/HelperFunctions.kt
@@ -1,6 +1,7 @@
 package com.example.tecnobank.extension
 
 import java.text.NumberFormat
+import java.text.SimpleDateFormat
 import java.util.*
 
 object HelperFunctions {
@@ -10,48 +11,11 @@ object HelperFunctions {
             .format(value)
     }
 
-    fun getDateMonthFormat(date: String): String {
-        return when {
-            date.contains("01/20") -> {
-                "${date.subSequence(0,2)} JAN"
-            }
-            date.contains("02/20") -> {
-                "${date.subSequence(0,2)} Fev"
-            }
-            date.contains("03/20") -> {
-                "${date.subSequence(0,2)} Mar"
-            }
-            date.contains("04/20") -> {
-                "${date.subSequence(0,2)} Abr"
-            }
-            date.contains("05/20") -> {
-                "${date.subSequence(0,2)} Mai"
-            }
-            date.contains("06/20") -> {
-                "${date.subSequence(0,2)} Jun"
-            }
-            date.contains("07/20") -> {
-                "${date.subSequence(0,2)} Jul"
-            }
-            date.contains("08/20") -> {
-                "${date.subSequence(0,2)} Ago"
-            }
-            date.contains("09/20") -> {
-                "${date.subSequence(0,2)} Set"
-            }
-            date.contains("10/20") -> {
-                "${date.subSequence(0,2)} Out"
-            }
-            date.contains("11/20") -> {
-                "${date.subSequence(0,2)} Nov"
-            }
-            date.contains("12/20") -> {
-                "${date.subSequence(0,2)} Dez"
-            }
-            else -> {
-                throw Exception("Data não encontrada")
-            }
-        }
-    }
-
+    fun getDateMonthFormat(date: String): String =
+        SimpleDateFormat("dd MMMM", Locale("pt", "BR")).format(
+            SimpleDateFormat(
+                "dd/MM/yyyy",
+                Locale("pt", "BR")
+            ).parse(date)
+        ).substring(0, 6).toUpperCase()
 }
