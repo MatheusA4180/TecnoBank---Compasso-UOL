1b20a08b285a05eff1fd6dc3203a3fe205d8dbc9
samuell <samuelltaylor@gmail.com>
2021-07-23 12:13:22 -0300

    integração das telas Pix através da navigation

--- app/src/main/java/com/example/tecnobank/home/fragments/services/PixInfoDestinationFragment.kt
+++ app/src/main/java/com/example/tecnobank/home/fragments/services/PixInfoDestinationFragment.kt
@@ -7,27 +7,28 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.tecnobank.R
-import com.example.tecnobank.databinding.PixTransferModeFragmentBinding
+import com.example.tecnobank.databinding.PixInfoDestinationFragmentBinding
 
-class PixTransferModeFragment: Fragment() {
+class PixInfoDestinationFragment: Fragment() {
 
-    private var _binding: PixTransferModeFragmentBinding? = null
-    private val binding: PixTransferModeFragmentBinding get() = _binding!!
+    private var _binding: PixInfoDestinationFragmentBinding? = null
+    private val binding: PixInfoDestinationFragmentBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = PixTransferModeFragmentBinding.inflate(inflater, container, false)
+        _binding = PixInfoDestinationFragmentBinding.inflate(inflater, container, false)
         return _binding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btEmailTransfer.setOnClickListener {
-         findNavController().navigate(R.id.action_pixTransferModeFragment_to_pixInfoDestinationFragment)
+        binding.btContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_pixInfoDestinationFragment_to_pixDescriptionFragment)
         }
     }
+
 }
