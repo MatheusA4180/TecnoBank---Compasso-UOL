d303dc574457d024872a60acc76a596a564e4418
Eduardo Augusto Oliveira <duduaugustooliveira@gmail.com>
2021-07-27 20:22:05 -0300

    teste dnv parte 3

--- app/src/main/java/com/example/tecnobank/extract/fragments/ExtractFragment.kt
+++ app/src/main/java/com/example/tecnobank/extract/fragments/ExtractFragment.kt
@@ -103,7 +103,7 @@ class ExtractFragment : Fragment() {
                 filterPosition = data.getIntExtra(FILTER_POSITION, 1)
             )
         }
-        //teste dnv parte 2
+        //teste dnv parte 3
     }
 
     private fun recyclerViewConfig(listExtracts: List<ExtractViewModel.ExtractItemAdapter>) {
