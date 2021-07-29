4243ecccbb3636e54ae45a0a1f8e6272e7135996
samuell <samuelltaylor@gmail.com>
2021-07-29 12:44:12 -0300

    correção pix activity

--- app/src/main/java/com/example/tecnobank/home/activity/services/PixQrCodeActivity.kt
+++ app/src/main/java/com/example/tecnobank/home/activity/services/PixQrCodeActivity.kt
@@ -1,5 +1,7 @@
 package com.example.tecnobank.home.activity.services
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -7,6 +9,8 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.tecnobank.R
+import com.example.tecnobank.data.local.SharedPreferenceServices
+import com.example.tecnobank.data.remote.EndPoint
 import com.example.tecnobank.databinding.PixQrCodeActivityBinding
 import com.example.tecnobank.home.viewmodel.PixViewModel
 import com.example.tecnobank.viewmodelfactory.ViewModelFactory
@@ -14,35 +18,41 @@ import com.example.tecnobank.viewmodelfactory.ViewModelFactory
 class PixQrCodeActivity : AppCompatActivity() {
 
     private lateinit var binding: PixQrCodeActivityBinding
-    private lateinit var viewModel: PixViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+        val sharedPreferenceServices = providerSharedPreferenceService(providerSharedPreference())
+
         binding = PixQrCodeActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel =
-            ViewModelProvider(this, ViewModelFactory(this))
-                .get(PixViewModel::class.java)
-
-        val navHostFragment = (supportFragmentManager.findFragmentById(R.id.navHostFragmentPix) as NavHostFragment)
+        val navHostFragment =
+            (supportFragmentManager.findFragmentById(R.id.navHostFragmentPix) as NavHostFragment)
         val inflater = navHostFragment.navController.navInflater
         val graph = inflater.inflate(R.navigation.pix_qr_code_nav_graph)
 
-        viewModel.passedOrNoPassedForPixOnbording()
-
-        viewModel.servicesToPixOnBoarding.observe(this, {
-            graph.startDestination = R.id.pixOnBordingFragment
-            //findNavController().navigate(R.id.action_homeFragment_to_pixOnBordingActivity)
-        })
-
-        viewModel.servicesToPix.observe(this, {
+        if(sharedPreferenceServices.passedByPixOnBoarding()) {
             graph.startDestination = R.id.pixQrCodeFragment
-            //findNavController().navigate(R.id.action_homeFragment_to_pixQrCodeActivity)
-        })
+
+        }else{
+            graph.startDestination = R.id.pixOnBordingFragment
+        }
 
         navHostFragment.navController.graph = graph
 
+    }
+
+    private fun providerSharedPreferenceService(
+        preferences: SharedPreferences
+    ): SharedPreferenceServices {
+        return SharedPreferenceServices(preferences)
+    }
+
+    private fun providerSharedPreference(): SharedPreferences {
+        return this.getSharedPreferences(
+            R.string.preference_file_key.toString(), Context.MODE_PRIVATE
+        )
     }
 }
